2cc889c653fb6d746b9e3a5a6acf5c562ccfc001
totoroJyunn@gmail.com <45234186+totoroJyunn@users.noreply.github.com>
2020-10-03 15:17:55 +0800

    撰写中

---
+++ a-2/a-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  a-2
+//
+//  Created by TotoroJ on 2020/10/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ a-2/a-2/a_2App.swift
@@ -0,0 +1,17 @@
+//
+//  a_2App.swift
+//  a-2
+//
+//  Created by TotoroJ on 2020/10/3.
+//
+
+import SwiftUI
+
+@main
+struct a_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
